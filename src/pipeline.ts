2ad48cf6ab1338cf9aac638a1e80a0b52e20bccb
kaizen3031593 <zen.conroy@gmail.com>
2022-02-10 14:27:29 -0500

    accountid as secret

--- src/pipeline.ts
+++ src/pipeline.ts
@@ -4,11 +4,16 @@ import { GitHubWorkflow } from 'cdk-pipelines-github';
 import { Construct } from 'constructs';
 import { WebsiteStack, WebsiteStackProps } from './website';
 
-const GITHUB_AWS_ACCOUNT_ID = '${{ secrets.AWS_ACCOUNT_ID }}';
+// prod account is stored as a secret and surfaced as env variable.
+// when testing, this env variable should be set locally so 'cdk synth' can succeed.
+// be aware, the value you set locally will be exposed in deploy.yml.
 export const BACKEND_ENV = {
-  account: process.env.AWS_ACCOUNT_ID ?? GITHUB_AWS_ACCOUNT_ID, // prod
+  account: process.env.AWS_ACCOUNT_ID, // prod
   region: 'us-east-1',
 };
+
+// prod account stored as secret.
+const GITHUB_AWS_ACCOUNT_ID = '${{ secrets.AWS_ACCOUNT_ID }}';
 const DOMAIN_NAME = 'cli.cdk.dev-tools.aws.dev';
 
 export class PipelineStack extends Stack {
