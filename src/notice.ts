c94f1c82900d7d02a24ee0a05ede8ec0efea6564
Otavio Macedo <otaviom@amazon.com>
2022-02-17 16:46:41 +0000

    chore: component name can be any string

--- src/notice.ts
+++ src/notice.ts
@@ -1,7 +1,6 @@
 import * as semver from 'semver';
 
 const MAX_TITLE_LENGTH = 100;
-const VALID_COMPONENT_NAMES = ['cli', 'framework'];
 
 export interface Component {
   name: string;
@@ -32,10 +31,6 @@ export function validateNotice(notice: Notice): void {
   }
 
   for (const component of notice.components) {
-    if (!VALID_COMPONENT_NAMES.includes(component.name)) {
-      throw new Error(`${component.name} is not a valid component name. Please choose one of ${VALID_COMPONENT_NAMES}`);
-    }
-
     if (!isValidSemverRange(component.version)) {
       throw new Error(`Component version ${component.version} is not a valid semver range`);
     }
