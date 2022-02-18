c914b77e2c5b0757e33f48d06449567e49de6612
Otavio Macedo <otaviomacedo@protonmail.com>
2022-02-18 15:25:22 +0000

    chore: component name can be any string

--- test/notices.test.ts
+++ test/notices.test.ts
@@ -26,19 +26,6 @@ test('rejects too long titles', () => {
   })).toThrow(/Maximum allowed title length/);
 });
 
-test('rejects invalid component names', () => {
-  expect(() => validateNotice({
-    title: 'Toggling off auto_delete_objects for Bucket empties the bucket',
-    issueNumber: 16603,
-    overview: 'If a stack is deployed with an S3 bucket with auto_delete_objects=True, and then re-deployed with auto_delete_objects=False, all the objects in the bucket will be deleted.',
-    components: [{
-      name: 'website',
-      version: '<=1.125.0',
-    }],
-    schemaVersion: '1',
-  })).toThrow(/is not a valid component name/);
-});
-
 test('rejects invalid component version range', () => {
   expect(() => validateNotice({
     title: 'Toggling off auto_delete_objects for Bucket empties the bucket',
@@ -63,6 +50,19 @@ test('rejects invalid schema version range', () => {
     }],
     schemaVersion: '.4',
   })).toThrow(/is not a valid semver range/);
+});
+
+test('accepts alpha version ranges', () => {
+  expect(() => validateNotice({
+    title: 'Toggling off auto_delete_objects for Bucket empties the bucket',
+    issueNumber: 16603,
+    overview: 'If a stack is deployed with an S3 bucket with auto_delete_objects=True, and then re-deployed with auto_delete_objects=False, all the objects in the bucket will be deleted.',
+    components: [{
+      name: 'cli',
+      version: '>= 2.1.0-alpha',
+    }],
+    schemaVersion: '1',
+  })).not.toThrow();
 });
 
 test('accepts empty notice array', () => {
