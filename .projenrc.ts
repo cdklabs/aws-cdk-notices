0c413912692d5c6d5394401c40b5be7a428343e4
Momo Kornher <kornherm@amazon.co.uk>
2022-11-30 14:15:33 +0000

    chore: use .projenrc.ts instead of .projenrc.js

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,5 +1,7 @@
-const { awscdk } = require('projen');
+import { awscdk } from 'projen';
+
 const project = new awscdk.AwsCdkTypeScriptApp({
+  projenrcTs: true,
   cdkVersion: '2.10.0',
   defaultReleaseBranch: 'main',
   name: 'aws-cdk-notices',
