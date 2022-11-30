1e460a394d43618b4c973c88b0445bdfbea57956
Momo Kornher <kornherm@amazon.co.uk>
2022-11-30 15:12:57 +0000

    fix: set min aws-cdk version to 2.51.0 so the OIDC thumbprint bug is definitely fixed (#25)
    Fixes the broken `deploy` workflow once deployed.
    Also migrates projen config to a ts file.

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,6 +1,8 @@
-const { awscdk } = require('projen');
+import { awscdk } from 'projen';
+
 const project = new awscdk.AwsCdkTypeScriptApp({
-  cdkVersion: '2.10.0',
+  projenrcTs: true,
+  cdkVersion: '2.51.0',
   defaultReleaseBranch: 'main',
   name: 'aws-cdk-notices',
 
