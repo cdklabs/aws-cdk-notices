14651f3c81664354f802d985ee1ca700b31d9fd6
Otavio Macedo <288203+otaviomacedo@users.noreply.github.com>
2023-03-16 09:48:31 +0000

    chore: notice for https://github.com/aws/aws-cdk/issues/24642

--- test/schema.test.ts
+++ test/schema.test.ts
@@ -41,7 +41,7 @@ describe('Notices file is valid', () => {
         for (const component of notice.components) {
           if (component.version === '1.*') { continue; } // Special range that we allow
           const range = new semver.Range(component.version);
-          if (range.test('1.999.0')) {
+          if (range.test('1.999.0') && range.test('2.0.0')) {
             throw new Error(`${component.version} should have an upper bound in v1 range, or a lower bound in v2 range (version should look like "^2.3.4 <2.5.6")`);
           }
         }
